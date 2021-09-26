5149410d7b2976dd97ca30ef7b26ae1797d1c189
Caleb Moses <calebjdmoses@gmail.com>
2021-09-27 01:52:21 +1300

    Add Pluto notebook

---
+++ Turing/intro-to-turing.jl
@@ -0,0 +1,22 @@
+### A Pluto.jl notebook ###
+# v0.16.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 46dfac7c-af28-4c54-9fce-be4a5bb97799
+using Random, Plots, Distributions, Turing, MCMCChains, StatsPlots
+	
+
+# ╔═╡ b580a470-1ec6-11ec-2239-4967f003e3d4
+md"# Intro to Turing.jl
+This is an introduction to `Turing.jl`.  Let's get started!
+"
+
+# ╔═╡ 3507deff-5772-4c39-8165-c065a192a982
+
+
+# ╔═╡ Cell order:
+# ╟─b580a470-1ec6-11ec-2239-4967f003e3d4
+# ╠═46dfac7c-af28-4c54-9fce-be4a5bb97799
+# ╠═3507deff-5772-4c39-8165-c065a192a982
